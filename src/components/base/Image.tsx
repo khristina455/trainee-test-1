9b01badf2289ae5bebc10f844cceef04385ef443
khristina455 <91967143+khristina455@users.noreply.github.com>
2024-03-13 19:28:28 +0300

    add image

--- src/components/base/Image.tsx
+++ src/components/base/Image.tsx
@@ -1,10 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
 
 export default (props) => {
-	return (
-		<img
-			alt=""
-			{...props}
-		/>
-	);
+	const { className, src, width, height } = props;
+	const ref = React.useRef(null);
+
+	React.useEffect(() => {
+		ref.current.style.backgroundImage = `url(${src})`;
+		ref.current.style.width = width + 'px';
+		ref.current.style.height = height + 'px';
+	}, []);
+	
+    return <div ref={ref} className={className}></div>;
 };
